088877ef03d175a5bbf7df4b0fac6898678abec4
Liberbell <liberbell@gmail.com>
2020-05-07 17:24:17 +0900

    views1

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -5,5 +5,16 @@ from django.http import HttpResponse
 def index(request):
     return render(request, "blog/index.html")
 
+posts = [
+    {'author': 'Lyncorn',
+     'title': 'Blog post 1',
+     'content': 'First post content',
+     'date_posted': 'May 7 2020'
+     },
+     {'author': 'Test',
+      'title': 'Blog post 2',
+      }
+]
+
 def business(request):
     return render(request, 'blog/business.html')
