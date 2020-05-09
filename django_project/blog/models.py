fb633f34a375b2ae619538667fb0a4267a73fd5b
Liberbell <liberbell@gmail.com>
2020-05-09 20:47:36 +0900

    models1

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -3,3 +3,23 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.class (models.Model):
+    tilte = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    
+
+    class Meta:
+        verbose_name = _("")
+        verbose_name_plura
+        tilte = models.CharField(_(""), max_length=50)l = _("s")
+        tilte = models.CharField(_(""), max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
+
+tilte = models.CharField(_(""), max_length=50))
