b1831e7acb4cf209d263af8a953ebabd3128bd8e
Liberbell <liberbell@gmail.com>
2020-05-09 20:49:02 +0900

    models3

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -8,19 +8,3 @@ class Post(models.class (models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    
-
-    class Meta:
-        verbose_name = _("")
-        verbose_name_plura
-        tilte = models.CharField(_(""), max_length=50)l = _("s")
-        tilte = models.CharField(_(""), max_length=50)
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse("_detail", kwargs={"pk": self.pk})
-
-tilte = models.CharField(_(""), max_length=50))
