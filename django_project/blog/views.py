969ecb2a3e7d96386d7649dc48c103ee43cb10af
Liberbell <liberbell@gmail.com>
2020-05-10 17:22:43 +0900

    views1

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -5,21 +5,23 @@ from django.http import HttpResponse
 def index(request):
     return render(request, "blog/index.html")
 
-posts = [
-    {'author': 'Lyncorn',
-     'title': 'Blog post 1',
-     'content': 'First post content',
-     'date_posted': 'May 7 2020'
-    },
-    {'author': 'Test',
-     'title': 'Blog post 2',
-     'content': 'Second post content',
-     'date_posted': 'May 8 2020'
-    }
-]
+# posts = [
+#     {'author': 'Lyncorn',
+#      'title': 'Blog post 1',
+#      'content': 'First post content',
+#      'date_posted': 'May 7 2020'
+#     },
+#     {'author': 'Test',
+#      'title': 'Blog post 2',
+#      'content': 'Second post content',
+#      'date_posted': 'May 8 2020'
+#     }
+# ]
 
 def business(request):
     context = {
-        'posts' : posts
+        # 'posts' : posts
+        'posts' : Post.objects.all()
+
     }
     return render(request, 'blog/business.html', context)
