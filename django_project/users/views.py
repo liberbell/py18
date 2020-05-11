267e31091811519ae6381e76e3d008d83a20a6f5
Liberbell <liberbell@gmail.com>
2020-05-11 16:53:43 +0900

    views2

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+    from = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
