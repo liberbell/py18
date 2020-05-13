95128baea621178ed12102ee1fc5b8cc4dcd6def
Liberbell <liberbell@gmail.com>
2020-05-13 17:16:19 +0900

    views2

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -4,5 +4,10 @@ from django.contrib import messages
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'users/register.html', {'form': form})
+    # form = UserCreationForm()
+    # return render(request, 'users/register.html', {'form': form})
+    if request.method == 'POST':
+        form = UserCreationForm(request.Post)
+
+    else:
+        form = UserCreationForm()
