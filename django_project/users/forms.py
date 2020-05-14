e6413bc3971e4ea2d88194ac34d41b5044eee1d3
Liberbell <liberbell@gmail.com>
2020-05-14 17:16:26 +0900

    forms3

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-class UserRegistrationForm(UserCreationForm):
+class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
