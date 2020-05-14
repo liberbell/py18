0f4150b0445e4e4a89973f94ff164a330d544a85
Liberbell <liberbell@gmail.com>
2020-05-14 16:41:19 +0900

    forms5

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -3,4 +3,9 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegistrationForm(UserCreationForm):
-    email = forms.EmailField(, required=False)
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password25
+        ']
